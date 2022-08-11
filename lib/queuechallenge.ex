43a20b14830c326c888b01eaf9721d2d92fa205d
hitk1 <luispaulo.degini@gmail.com>
2022-08-10 21:51:11 -0300

    initial commit

---
+++ lib/queuechallenge.ex
@@ -0,0 +1,18 @@
+defmodule Queuechallenge do
+  @moduledoc """
+  Documentation for `Queuechallenge`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Queuechallenge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/queuechallenge_test.exs
@@ -0,0 +1,8 @@
+defmodule QueuechallengeTest do
+  use ExUnit.Case
+  doctest Queuechallenge
+
+  test "greets the world" do
+    assert Queuechallenge.hello() == :world
+  end
+end
